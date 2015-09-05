18456a9054f98c8ca4739a3f4cf789d0b1afc823
Lambros Tabourlos <tab.lambros@gmail.com>
2015-09-05 17:01:01 +0300

    fix

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var uuid = require('uuid');
 
 module.exports = {
-	'rootDir': '/Applications/MAMP/htdocs/CocktailServer',
+	'rootDir': '.',
 	'modelDir': '/models/',
 	'routeDir': '/routes/',
     'secret': uuid.v4(),
